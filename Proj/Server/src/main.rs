caf5457f05ad53046c7c826387c786005aa2ad3f
Salma Zaghloul <salma.zaghloul@aucegypt.edu>
2023-10-24 20:34:23 +0300

    changed Server file

--- Proj/Server/src/main.rs
+++ Proj/Server/src/main.rs
@@ -3,7 +3,7 @@ use tokio::net::UdpSocket;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let local_addr = "127.0.0.1:8080"; // IP address and port you want the server (this process) to listen on
+    let local_addr = "0.0.0.0:8082"; // IP address and port you want the server (this process) to listen on
     let socket = UdpSocket::bind(local_addr).await?;
     let mut buffer = [0; 1024]; // Buffer to receive the message
 
@@ -12,5 +12,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         let message = std::str::from_utf8(&buffer[..len])?;
 
         println!("Received: {} from {}", message, src);
+
+        // Send a response back to the client
+        let response = "Hello, client!"; // Change this to your response message
+        let sent_len = socket.send_to(response.as_bytes(), &src).await?;
+        println!("Sent: {} bytes to {}", sent_len, src);
     }
 }
