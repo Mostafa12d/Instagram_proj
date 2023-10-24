ab0855c8e8dd6fec925d7bb3aa5c028c0ed1d60d
Mostafa <mostafamibrahim12@gmail.com>
2023-10-24 19:34:14 +0300

    added easier code

--- Project/hello_world/target/debug/hello_world.d
+++ Project/hello_world/target/debug/hello_world.d
@@ -1,1 +1,1 @@
-/Users/mostafalotfy/Documents/University/Fall\ 2023/Distributed\ Systems/Project/hello_world/target/debug/hello_world: /Users/mostafalotfy/Documents/University/Fall\ 2023/Distributed\ Systems/Project/hello_world/src/main.rs
+/Users/mostafalotfy/Documents/University/Fall\ 2023/Distributed\ Systems/Instagram_proj/Project/hello_world/target/debug/hello_world: /Users/mostafalotfy/Documents/University/Fall\ 2023/Distributed\ Systems/Instagram_proj/Project/hello_world/src/main.rs
--- Project/sockets/target/debug/sockets.d
+++ Project/sockets/target/debug/sockets.d
@@ -1,1 +1,1 @@
-/Users/mostafalotfy/Documents/University/Fall\ 2023/Distributed\ Systems/Project/sockets/target/debug/sockets: /Users/mostafalotfy/Documents/University/Fall\ 2023/Distributed\ Systems/Project/sockets/src/main.rs
+/Users/mostafalotfy/Documents/University/Fall\ 2023/Distributed\ Systems/Instagram_proj/Project/sockets/target/debug/sockets: /Users/mostafalotfy/Documents/University/Fall\ 2023/Distributed\ Systems/Instagram_proj/Project/sockets/src/main.rs
