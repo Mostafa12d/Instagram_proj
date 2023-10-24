6745924b1243d5cc85e03dc356f3b5b5ed350ba1
Mostafa <mostafamibrahim12@gmail.com>
2023-10-24 20:48:53 +0300

    reversed uno

--- Proj/Client/src/main.rs
+++ Proj/Client/src/main.rs
@@ -1,16 +1,12 @@
+// This is the client
 use tokio::net::UdpSocket;
-use std::fs::File;
-use std::io::{Read, Result};
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let remote_addr = "127.0.0.1:8080"; // IP address and port of Node 2
+    let remote_addr = "127.0.0.1:8080"; // IP address and port of the Server
+
     let socket = UdpSocket::bind("0.0.0.0:0").await?;
     let message = "Hello, World!";
-    
-    //Read the picture and change it to a vector of bytes
-    let mut file = File::open("tremer.png")?;
-    let mut picture_data = Vec::new();
-    file.read_to_end(&mut picture_data)?;
     let message_bytes = message.as_bytes();
 
     socket.send_to(message_bytes, remote_addr).await?;
@@ -19,6 +15,3 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     Ok(())
 }
-
-
-
--- Proj/Server/src/main.rs
+++ Proj/Server/src/main.rs
@@ -3,23 +3,19 @@ use tokio::net::UdpSocket;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let local_addr = "127.0.0.1:8080"; // IP address and port you want the server (this process) to listen on
+    let local_addr = "0.0.0.0:8082"; // IP address and port you want the server (this process) to listen on
     let socket = UdpSocket::bind(local_addr).await?;
-    let mut buffer = vec![0; 4096]; // Buffer to receive the message
-    let (num_bytes, _) = socket.recv_from(&mut buf)?;
-    let image_data = &buf[..num_bytes];
-    
-    let decoder = PngDecoder::new(std::io::Cursor::new(image_data));
-    let (width, height) = decoder.dimensions();
-    let image = decoder.read_image()?;
-
-    // Save or display the reconstructed image
-    image.save("reconstructed_image.png")?;
+    let mut buffer = [0; 1024]; // Buffer to receive the message
 
     loop {
         let (len, src) = socket.recv_from(&mut buffer).await?;
         let message = std::str::from_utf8(&buffer[..len])?;
 
         println!("Received: {} from {}", message, src);
+
+        // Send a response back to the client
+        let response = "Hello, client!"; // Change this to your response message
+        let sent_len = socket.send_to(response.as_bytes(), &src).await?;
+        println!("Sent: {} bytes to {}", sent_len, src);
     }
 }
