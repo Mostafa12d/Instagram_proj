0a65ca2ee70df7ff824882e49f65b04adbe724b3
Mostafa <mostafamibrahim12@gmail.com>
2023-10-24 19:56:49 +0300

    image sending

--- Proj/hello/src/main.rs
+++ Proj/hello/src/main.rs
@@ -5,7 +5,16 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let local_addr = "127.0.0.1:8080"; // IP address and port you want Node 2 to listen on
 
     let socket = UdpSocket::bind(local_addr).await?;
-    let mut buffer = [0; 1024]; // Buffer to receive the message
+    let mut buffer = vec![0; 4096]; // Buffer to receive the message
+    let (num_bytes, _) = socket.recv_from(&mut buf)?;
+    let image_data = &buf[..num_bytes];
+    
+    let decoder = PngDecoder::new(std::io::Cursor::new(image_data));
+    let (width, height) = decoder.dimensions();
+    let image = decoder.read_image()?;
+
+    // Save or display the reconstructed image
+    image.save("reconstructed_image.png")?;
 
     loop {
         let (len, src) = socket.recv_from(&mut buffer).await?;
--- Proj/node2/src/main.rs
+++ Proj/node2/src/main.rs
@@ -1,11 +1,16 @@
 use tokio::net::UdpSocket;
-
+use std::fs::File;
+use std::io::{Read, Result};
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let remote_addr = "127.0.0.1:8080"; // IP address and port of Node 2
-
     let socket = UdpSocket::bind("0.0.0.0:0").await?;
     let message = "Hello, World!";
+    
+    //Read the picture and change it to a vector of bytes
+    let mut file = File::open("tremer.png")?;
+    let mut picture_data = Vec::new();
+    file.read_to_end(&mut picture_data)?;
     let message_bytes = message.as_bytes();
 
     socket.send_to(message_bytes, remote_addr).await?;
