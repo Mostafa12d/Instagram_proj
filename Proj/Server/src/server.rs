890b6f585d0e8dd5ba1b82d1ee308f5dc55a7b8b
Mostafa <mostafamibrahim12@gmail.com>
2023-11-01 14:55:45 +0200

    new branch

--- Proj/Server/src/server.rs
+++ Proj/Server/src/server.rs
@@ -2,6 +2,7 @@ use tokio::net::UdpSocket;
 use std::fs::{File, OpenOptions};
 use std::io::{BufReader, BufRead, Write};
 use std::error::Error;
+use std::string;
 use rand::seq::SliceRandom;
  // to identify the ip address of the machine this code is running on
 use local_ip_address::local_ip;
@@ -19,22 +20,22 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     // Get Port from Command Line
     let args: Vec<String> = std::env::args().collect();
-    let portNum = args.get(1).expect("Argument 1 is listening port. Eg: 8080");
-    println!("{}", portNum);
+    let port_num = args.get(1).expect("Argument 1 is listening port. Eg: 8080");
+    println!("{}", port_num);
     //Function finds the ip of the running server
     let local_ip = local_ip().unwrap(); // Get the dynamically assigned IP address
     // Create a server
-    let local_addr = local_ip.to_string()+":"+portNum;
+    let local_addr = local_ip.to_string()+":"+port_num;
     println!("{}", local_addr);
 
     let server_info = ServerInfo {
         ip: local_ip.to_string(), // Set the server's IP address
-        port: portNum.to_string(),  //the server's port
+        port: port_num.to_string(),  //the server's port
         status: 1, // Set the server's status
     };
 
     // Append server information to a txt file
-    append_server_info_to_file(&server_info)?;
+    // append_server_info_to_file(&server_info)?;
     // Start the server
     start_server(&server_info).await?;
 
@@ -54,24 +55,15 @@ impl ServerInfo {
 }
 
 // Append server information to a txt file
-fn append_server_info_to_file(info: &ServerInfo) -> Result<(), Box<dyn Error>> {
+fn get_server_info(filename: &str) -> Result<(), Box<dyn Error>> {
 
-    let filename = "/home/tamer/DS/Instagram_proj/Proj/Server/src/DoSS.txt"; 
-    let mut file = OpenOptions::new()
-        .create(true)
-        .append(true)
-        .open(filename)?;
-
-    // Read the existing contents of the file into a string
-    let file_contents = std::fs::read_to_string(filename)?;
-
-    // Check if the server info already exists in the file
-    if file_contents.contains(&format!("IP: {}, Port: {}", info.ip, info.port)) {
-        println!("Server info already exists in the file. Skipping...");
-    } else {
-        let line = format!("IP: {}, Port: {}, Status: {}\n", info.ip, info.port, info.status);
-        file.write(line.as_bytes()).expect("write failed");
-        println!("Server info added to the file.");
+    let file = File::open(filename).expect("no such file");
+    let buf = BufReader::new(file);
+    let local_addr_v: Vec<String> = buf.lines()
+    .map(|l| l.expect("Could not parse line"))
+    .collect();
+    for addr in &local_addr_v{
+        println!("{}", addr);
     }
 
     Ok(())
@@ -80,14 +72,16 @@ fn append_server_info_to_file(info: &ServerInfo) -> Result<(), Box<dyn Error>> {
 // Start the server
 async fn start_server(server_info: &ServerInfo) -> Result<(), Box<dyn Error>> {
     let local_addr = format!("{}:{}", server_info.ip, server_info.port);
-    let socket = UdpSocket::bind(&local_addr).await?;
+    let client_socket = UdpSocket::bind(&local_addr).await?;
+
+    let server_socket = UdpSocket::bind(&local_addr).await?;
+
 
 
     // 
 
-    let mut buffer = [0; 1024];
-    let mut buffer1 = [0; 1024];
-    let mut buffer2 = [0; 1024];
+    let mut client_buffer = [0; 1024];
+    let mut server_buffer = [0; 1024];
 
     
 
@@ -97,78 +91,75 @@ async fn start_server(server_info: &ServerInfo) -> Result<(), Box<dyn Error>> {
     println!("This server is listening on: {}", local_addr);
 
     loop {
-        let (len, client) = socket.recv_from(&mut buffer).await?;
-        let message = std::str::from_utf8(&buffer[..len])?;
-        // if (len)
-        // {
-
-        // }
+        let (len, client) = client_socket.recv_from(&mut client_buffer).await?;
+        let message = std::str::from_utf8(&client_buffer[..len])?;
+        println!("Received: {} from {}", message, client);
 
         //// Draft Leader Election
-        // Receive from other servers their status aka priority.
-        let (len1, server1) = socket.recv_from(&mut buffer1).await?;
-        let message1 = std::str::from_utf8(&buffer1[..len1])?;
+        // // Receive from other servers their status aka priority.
+        // let (len1, server1) = socket.recv_from(&mut buffer1).await?;
+        // let message1 = std::str::from_utf8(&buffer1[..len1])?;
 
-        let (len2, server2) = socket.recv_from(&mut buffer2).await?;
-        let message2 = std::str::from_utf8(&buffer2[..len2])?;
+        // let (len2, server2) = socket.recv_from(&mut buffer2).await?;
+        // let message2 = std::str::from_utf8(&buffer2[..len2])?;
 
         //check which server is free using some ifs
 
         //send token = ok to the server that has the least priority
         println!("Received: {} from {}", message, client);
-        println!("Received: {} from {}", message1, server1);
-        println!("Received: {} from {}", message2, server2);
-
-
-        // Check the server status and respond or delegate the task
-        match server_info.status {
-            0 => {
-                // If status is 0 (inactive), delegate to another active server
-                let delegate_server = find_active_server(); // Implement your logic to find an active server
-                if let Ok(Some(delegate_server)) = delegate_server {
-                    let delegate_addr = format!("{}:{}", delegate_server.ip, delegate_server.port);
-                    let response = "Task delegated to another server.";
-                    let _ = socket.send_to(response.as_bytes(), &delegate_addr).await;
-                    println!("Task delegated to {}.", delegate_addr);
-                } else {
-                    println!("No active server available to delegate the task.");
-                }
-            }
-            1 => {
-                // If status is 1 (active), respond to the client
-                // Set the status to busy
-                if server_info.status == 1 {
-                    set_server_status(&server_info, 2)?;
-                    println!("---------------------------------------------------");
-                    println!("Server is busy.");
-                    // Print server information
-                    println!("Server is running with the following info:");
-                    println!("IP: {}, Port: {}, Status: {}", server_info.ip, server_info.port, server_info.status);
-                    println!("---------------------------------------------------");
-                }
-                
-                let response = "Hello, client!";
-                let sent_len = socket.send_to(response.as_bytes(), &client).await?;
-                println!("Sent: {} bytes to {}", sent_len, client);
-
-                // Set the status back to active
-                if server_info.status == 2 {
-                    set_server_status(&server_info, 1)?;
-                }
-
-                println!("Server is active again.");
-            }
-
-            2 => {
-                // If status is 2 (busy), log that the server is busy
-                println!("Server is busy. No response sent.");
-            }
-            _ => {
-                // Handle other status values as needed
-                println!("Invalid server status. No response sent.");
-            }
-        }
+        // println!("Received: {} from {}", message1, server1);
+        // println!("Received: {} from {}", message2, server2);
     }
+
+        // // Check the server status and respond or delegate the task
+        // match server_info.status {
+        //     0 => {
+        //         // If status is 0 (inactive), delegate to another active server
+        //         let delegate_server = find_active_server(); // Implement your logic to find an active server
+        //         if let Ok(Some(delegate_server)) = delegate_server {
+        //             let delegate_addr = format!("{}:{}", delegate_server.ip, delegate_server.port);
+        //             let response = "Task delegated to another server.";
+        //             let _ = client_socket.send_to(response.as_bytes(), &delegate_addr).await;
+        //             println!("Task delegated to {}.", delegate_addr);
+        //         } else {
+        //             println!("No active server available to delegate the task.");
+        //         }
+        //     }
+        //     1 => {
+        //         // If status is 1 (active), respond to the client
+        //         // Set the status to busy
+        //         if server_info.status == 1 {
+        //             set_server_status(&server_info, 2)?;
+        //             println!("---------------------------------------------------");
+        //             println!("Server is busy.");
+        //             // Print server information
+        //             println!("Server is running with the following info:");
+        //             println!("IP: {}, Port: {}, Status: {}", server_info.ip, server_info.port, server_info.status);
+        //             println!("---------------------------------------------------");
+        //         }
+                
+    //             let response = "Hello, client!";
+    //             let sent_len = client_socket.send_to(response.as_bytes(), &client).await?;
+    //             println!("Sent: {} bytes to {}", sent_len, client);
+
+    //             // Set the status back to active
+    //             if server_info.status == 2 {
+    //                 set_server_status(&server_info, 1)?;
+    //             }
+
+    //             println!("Server is active again.");
+    //         }
+
+    //         2 => {
+    //             // If status is 2 (busy), log that the server is busy
+    //             println!("Server is busy. No response sent.");
+    //         }
+    //         _ => {
+    //             // Handle other status values as needed
+    //             println!("Invalid server status. No response sent.");
+    //         }
+    //     }
+    // }
 
     Ok(())
 }
